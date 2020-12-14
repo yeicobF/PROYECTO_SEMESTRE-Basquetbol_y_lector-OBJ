5a1aca4cc4ccd495792093137fe362a3acc2fda8
yeicobF <A291423@alumnos.uaslp.mx>
2020-12-14 11:39:51 -0600

    ARCHIVOS Y ORDENACIÓN CARPETAS MODIFICADOS

--- LECTOR OBJ/main.cpp
+++ LECTOR OBJ/main.cpp
@@ -24,7 +24,7 @@ using namespace std;
 
 int main(){
     // Utilizar el constructor con el nombre del archivo.
-    Object obj = Object("modelsOBJ/OrangeCartoon_basketball_ball_OBJ.obj");
+    Object obj = Object("modelsOBJ/roca.obj");
     obj.saveObject();
     // std::cout << "\n Terminó de guardar el objeto.\n";
     obj.printObject();
